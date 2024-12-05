529f557be6d2e0db250428a7cc1b24e2c3c89f6b
Faias46 <nagurfaias@gmail.com>
2024-12-05 22:29:43 +0530

    Update provider.tf

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -9,12 +9,12 @@ terraform {
   backend "s3" {
     bucket = "81s-remote-state-dev"
     key    = "expense-vpc"
-    region = "eu-north-1"
+    region = "us-east-1"
     dynamodb_table = "81s-locking-dev"
   }
 }
 
 provider "aws" {
   # Configuration options
-  region = "eu-north-1"
+  region = "us-east-1"
 }
